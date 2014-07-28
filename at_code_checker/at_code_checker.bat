66db0998b29edb7fbf0fe7ab2b103b428e30507b
Kenny Baskett <kbaskett248@gmail.com>
2014-07-28 10:28:38 -0400

    Updating AT Code Checker to latest release - 2014.07.25 "Orion"

--- at_code_checker/at_code_checker.bat
+++ at_code_checker/at_code_checker.bat
@@ -1,6 +1,5 @@
 @echo off
 rem Update Focus Tools and then execute the code checker passing the first argument.
 rem
-rem TODO: remove the 'Test' in "M-AT Tools Test" before releasing.
-"%ProgramFiles%\MEDITECH\MTAppDwn.exe" -silent -update "C:\Program Files\MEDITECH\M-AT Tools Test\Client.mtad"
-"%ProgramFiles%\MEDITECH\M-AT Tools Test\M-AT_Code_Checker\at_code_checker.exe" %1
+"%ProgramFiles%\MEDITECH\MTAppDwn.exe" -silent -update "%ProgramFiles%\MEDITECH\M-AT Tools\Client.mtad"
+"%ProgramFiles%\MEDITECH\M-AT Tools Test\M-AT_Code_Checker\at_code_checker.exe" %*
