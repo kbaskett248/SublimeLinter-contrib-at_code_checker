4e166e9320c9512c54077d436fbaacef28b5dad5
Kenny Baskett <kbaskett248@gmail.com>
2014-07-29 13:29:25 -0400

    Updating AT Code Checker to latest release - 2014.07.29

--- at_code_checker/at_code_checker.bat
+++ at_code_checker/at_code_checker.bat
@@ -2,4 +2,4 @@
 rem Update Focus Tools and then execute the code checker passing the first argument.
 rem
 "%ProgramFiles%\MEDITECH\MTAppDwn.exe" -silent -update "%ProgramFiles%\MEDITECH\M-AT Tools\Client.mtad"
-"%ProgramFiles%\MEDITECH\M-AT Tools Test\M-AT_Code_Checker\at_code_checker.exe" %*
+"%ProgramFiles%\MEDITECH\M-AT Tools\M-AT_Code_Checker\at_code_checker.exe" %*
