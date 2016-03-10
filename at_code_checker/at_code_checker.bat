f13742724f2adb06e490556efd907dd52eb77328
Kenny Baskett <kbaskett248@gmail.com>
2016-03-10 07:35:47 -0500

    Updating AT Code Checker to v2016.03.07 "Helium"

--- at_code_checker/at_code_checker.bat
+++ at_code_checker/at_code_checker.bat
@@ -1,5 +1,5 @@
 @echo off
 rem Update Focus Tools and then execute the code checker passing the first argument.
 rem
-"%ProgramFiles%\MEDITECH\MTAppDwn.exe" -silent -update "%ProgramFiles%\MEDITECH\M-AT Tools\Client.mtad"
-"%ProgramFiles%\MEDITECH\M-AT Tools\M-AT_Code_Checker\at_code_checker.exe" %*
+"C:\Program Files (x86)\MEDITECH\MTAppDwn.exe" -silent -update "C:\Program Files (x86)\MEDITECH\M-AT Tools\Client.mtad"
+"C:\Program Files (x86)\MEDITECH\M-AT Tools\M-AT_Code_Checker\at_code_checker.exe" %*
